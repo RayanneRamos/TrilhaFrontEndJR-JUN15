275e7c508c7b3721c063156035002b76e8a1e529
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-06-22 17:35:43 -0300

    :sparkles: feat: dynamically creating the hobbies section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-console.log("oi");
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("hobbies.json")
+    .then((response) => response.json())
+    .then((hobbie) => {
+      const hobbiesContent = document.getElementById("hobbies-content");
+
+      hobbie.forEach((hobbie) => {
+        const cardContainer = document.createElement("div");
+        cardContainer.className = "hobbies-card-container";
+
+        const cardContent = document.createElement("div");
+        cardContent.className = "hobbies-card-content";
+
+        const img = document.createElement("img");
+        img.src = hobbie.hobbieDescription;
+        img.className = "hobbies-card-image";
+        img.alt = hobbie.hobbieName;
+
+        const cardInfo = document.createElement("div");
+        cardInfo.className = "hobbies-card-info";
+
+        const cardTitle = document.createElement("strong");
+        cardTitle.className = "hobbies-card-title";
+        cardTitle.innerText = hobbie.hobbieName;
+
+        cardInfo.appendChild(cardTitle);
+        cardContent.appendChild(img);
+        cardContent.appendChild(cardInfo);
+        cardContainer.appendChild(cardContent);
+        hobbiesContent.appendChild(cardContainer);
+      });
+    });
+});
